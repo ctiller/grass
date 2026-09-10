38536c7eb8cbb2913573841be0feddd3c1adb303
Craig Tiller <craig.tiller@gmail.com>
2026-09-09 17:40:30 -0700

    Connect WriteFile return history to source body lowering

--- Tools/AxiomAudit.lean
+++ Tools/AxiomAudit.lean
@@ -146,6 +146,7 @@ import Grass.Refinement.Console.WriteAllHead
 import Grass.Refinement.Console.WriteAllFactory
 import Grass.Refinement.Console.WriteAllBody
 import Grass.Refinement.Console.WriteFileCountAddress
+import Grass.Refinement.Console.WriteFileResume
 import Grass.Refinement.Console.WriteAllGuards
 import Grass.Refinement.Console.WriteFileLoad
 import Grass.Assembly.SourceInput
